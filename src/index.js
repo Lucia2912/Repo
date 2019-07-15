dccf8f06d55b5b1ef1740f7ca45ee35c0095fe49
Lucia <luciaurrutty@gmail.com>
2019-07-15 20:10:36 -0300

    uuh

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const app = express();
 require('./lib/passport');
 
 //Configuraciones
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
 	defaultLayout: 'main',
